073e5f602a8e05f708249f2c6a5c81253b27f254
MP-Alumno <MP-Alumno@duoc.acad>
2026-09-10 12:09:12 -0300

    feat: add Spring Boot backend

---
+++ backend/src/test/java/com/cloudnative/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnative.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
